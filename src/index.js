a2c3493e77656020dff8c2d929057460d3f3e813
CodingRuslan <orion.hola@gmail.com>
2019-03-05 22:04:41 +0300

    feat: solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let d = [];
+  let open = [];
+  let close = [];
+  let exept = [];
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (bracketsConfig[i][0] !== bracketsConfig[i][1]) {
+      open.push(bracketsConfig[i][0]);
+      close.push(bracketsConfig[i][1]);
+    }else{
+      exept.push(bracketsConfig[i][0]);
+    }
+  }
+  for (let i = 0; i < str.length; i++) {
+    if (open.indexOf(str[i]) !== -1) {
+      d.push(str[i]);
+    }else if(exept.indexOf(str[i]) !== -1){
+      if (d.indexOf(str[i]) !== -1) {
+        if (d[d.length-1] === str[i]) {
+          d.pop();
+        }else{
+          return false;
+        }
+      }else{
+        d.push(str[i]);
+      }
+    } else{
+      let s = close.indexOf(str[i]);
+      if (open[s] === d[d.length - 1]) {
+        d.pop();
+      } else{
+        return false;
+      }
+    }
+  }
+  if (d.length === 0) {
+    return true;
+  } else{
+    return false;
+  }
+  
 }
